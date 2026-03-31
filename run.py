06b8a94147d842cbd60016fdfe49a43241ccbeab
MOS2025 <michealose@gmail.com>
2026-03-31 15:23:11 +0100

    Configured APis and confirmed access to google sheets

--- run.py
+++ run.py
@@ -1,1 +1,20 @@
-print("Hello, world!")
+#print("Hello, world!")
+import gspread
+from google.oauth2.service_account import Credentials
+
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+    ]
+
+CREDS = Credentials.from_service_account_file('creds.json')
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+SHEET = GSPREAD_CLIENT.open('Electricity_Prices')
+
+Sheet1 = SHEET.worksheet('Sheet1')
+
+data = Sheet1.get_all_values()
+
+print(data)
